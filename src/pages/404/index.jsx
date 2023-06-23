48fd91a841fe3e358b49ed923011a6ce1a9567a7
Akain <akain1612@gmail.com>
2023-06-24 00:47:39 +0200

    Update error page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom'
+
 function Error() {
-    return <div>Error</div>
+    return (
+        <main className="error">
+            <h2 className="error__title">404</h2>
+            <p className="error__subtitle">
+                Oups! La page que vous demandez n'existe pas.
+            </p>
+            <Link to={'/'} className="error__link">
+                Retourner sur la page d'accueil
+            </Link>
+        </main>
+    )
 }
 
 export default Error
